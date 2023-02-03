70665d5297cd1a84afe49bd5d4f7aee9d2a0f523
primelost <80356414+nana444ahahhh@users.noreply.github.com>
2023-02-03 19:47:28 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import pygame
-import sys
 import os
+import sys
+
+import pygame
 from pygame.sprite import Group
-from pygame import mixer
 import random
+
 all_sprites = pygame.sprite.Group()
 schr = 0
 schu = 0
@@ -34,7 +35,8 @@ class mobs(pygame.sprite.Sprite):
     def __init__(self, screen):
         super(mobs, self).__init__()
         self.screen = screen
-        self.image = load_image('monster.png')
+        ls = ['monster.png', 'octo.png']
+        self.image = load_image(random.choice(ls))
         self.rect = self.image.get_rect()
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
@@ -64,6 +66,27 @@ class bombs(pygame.sprite.Sprite):
     def update(self):
         self.y_pos -= self.speed
         self.rect.y = self.y_pos
+
+    def realise(self):
+        self.screen.blit(self.image, self.rect)
+
+
+class health:
+    def __init__(self, screen, eall):
+        self.number = hp
+        self.screen = screen
+        self.images = ['3hearts.png', '2hearts.png', '1heart.png']
+        if self.number == 3:
+            self.image = load_image(self.images[0])
+            self.rect = self.image.get_rect()
+        elif self.number == 2:
+            self.image = load_image(self.images[1])
+            self.rect = self.image.get_rect()
+        else:
+            self.image = load_image(self.images[2])
+            self.rect = self.image.get_rect()
+        self.rect.top = self.screen.get_rect().top
+        self.rect.left = self.screen.get_rect().left
 
     def realise(self):
         self.screen.blit(self.image, self.rect)
@@ -106,13 +129,18 @@ def load_image(name, colorkey=None, transform=None):
     else:
         image = image.convert_alpha()
     return image
+
+
 def start_screen():
-    screen.fill((0,0,0))
-    print("ads")
-    if running == True:
-        print("asd")
-    a = 1
-    intro_text = ["главный экран", "это главный экран", "да-да, главный экран"]
+    playlist.append("data/ehh.mp3")
+    playlist.append("data/ehh.mp3")
+
+    pygame.mixer.music.load(playlist.pop())
+    pygame.mixer.music.queue(playlist.pop())
+    pygame.mixer.music.set_endevent(pygame.USEREVENT)
+    pygame.mixer.music.play()
+    print(running)
+    intro_text = ["нажмите w", "чтобы начать заново"]
     fon = pygame.transform.scale(load_image('game over.jpg'), (1200, 720))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
@@ -125,9 +153,27 @@ def start_screen():
         intro_rect.x = 10
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
+    pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    playlist.clear()
+                    playlist.append("data/music.mp3")
+                    playlist.append("data/music.mp3")
+
+                    pygame.mixer.music.load(playlist.pop())
+                    pygame.mixer.music.queue(playlist.pop())
+                    pygame.mixer.music.set_endevent(pygame.USEREVENT)
+                    pygame.mixer.music.play()
+
+                    return
+
 
 if __name__ == '__main__':
-
+    print("wfdwe")
     pygame.init()
     pygame.display.set_caption('siv')
     size = width, height = 1200, 720
@@ -142,8 +188,8 @@ if __name__ == '__main__':
     m = mobs(screen)
     wd = m.rect.width
     ht = m.rect.height
-    kol = 20
     c = 0
+    rows = 1
     clock = pygame.time.Clock()
     playlist = list()
     playlist.append("data/music.mp3")
@@ -154,14 +200,14 @@ if __name__ == '__main__':
     pygame.mixer.music.set_endevent(pygame.USEREVENT)
     pygame.mixer.music.play()
     hp = 3
-    a = 0
+    b = False
     f1 = pygame.font.Font(None, 36)
-    text1 = f1.render(f'жизни:{hp}', True,
-                      (180, 0, 0))
+
     wave = 0
-
-
-    for s in range(5):
+    schu = 0
+    hl = health(screen, hp)
+    st = True
+    for s in range(rows):
         for i in range(22):
             m = mobs(screen)
             m.x = wd + wd * i
@@ -169,97 +215,98 @@ if __name__ == '__main__':
             m.rect.x = m.x
             m.rect.y = m.y
             monsters.add(m)
-    while a == 0:
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.USEREVENT:
-                    if len(playlist) > 1:
-                        pygame.mixer.music.queue(playlist.pop())
+    rows += 1
 
-                if event.type == pygame.QUIT:
-                    running = False
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_d:
-                        schr = 1
-                    if event.key == pygame.K_s:
-                        schd = 1
-                    if event.key == pygame.K_a:
-                        schl = 1
-                    if event.key == pygame.K_w:
-                        schu = 1
-                    if event.key == pygame.K_SPACE:
-                        material = bombs(screen, shp)
-                        many_bombs.add(material)
-                if event.type == pygame.KEYUP:
-                    if event.key == pygame.K_d:
-                        schr = 0
-                    if event.key == pygame.K_s:
-                        schd = 0
-                    if event.key == pygame.K_a:
-                        schl = 0
-                    if event.key == pygame.K_w:
-                        schu = 0
-            if schr == 1 and shp.rect.right <= 1220:
-                shp.cc += 1
-            if schd == 1 and shp.rect.bottom < 720:
-                shp.cy += 1
-            if schl == 1 and shp.rect.left > -20:
-                shp.cc -= 1
-            if schu == 1 and shp.rect.top > -20:
-                shp.cy -= 1
-            shp.rect.centerx = shp.cc
-            shp.rect.centery = shp.cy
+    while running:
 
-            shp.draw()
-            many_bombs.update()
-            for i in many_bombs.copy():
-                if i.rect.top == 50:
-                    i.image = load_image('explosion.png')
-                    print("qw")
-                if i.rect.top < 0:
-                    many_bombs.remove(i)
-            for i in many_bombs.sprites():
-                i.realise()
-            goto = pygame.sprite.groupcollide(many_bombs, monsters, True, True)
+        for event in pygame.event.get():
+            if event.type == pygame.USEREVENT:
+                if len(playlist) > 1:
+                    pygame.mixer.music.queue(playlist.pop())
+            if event.type == pygame.QUIT:
+                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    schr = 1
+                if event.key == pygame.K_s:
+                    schd = 1
+                if event.key == pygame.K_a:
+                    schl = 1
+                if event.key == pygame.K_w:
+                    schu = 1
+                if event.key == pygame.K_SPACE:
+                    material = bombs(screen, shp)
+                    many_bombs.add(material)
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_d:
+                    schr = 0
+                if event.key == pygame.K_s:
+                    schd = 0
+                if event.key == pygame.K_a:
+                    schl = 0
+                if event.key == pygame.K_w:
+                    schu = 0
+        if schr == 1 and shp.rect.right <= 1220:
+            shp.cc += 1
+        if schd == 1 and shp.rect.bottom < 720:
+            shp.cy += 1
+        if schl == 1 and shp.rect.left > -20:
+            shp.cc -= 1
+        if schu == 1 and shp.rect.top > -20:
+            shp.cy -= 1
+        shp.rect.centerx = shp.cc
+        shp.rect.centery = shp.cy
+        shp.draw()
+        many_bombs.update()
+        for i in many_bombs.copy():
+            if i.rect.top == 50:
+                i.image = load_image('explosion.png')
+                print("qw")
+            if i.rect.top < 0:
+                many_bombs.remove(i)
+        for i in many_bombs.sprites():
+            i.realise()
+        goto = pygame.sprite.groupcollide(many_bombs, monsters, True, True)
+        if c % 20 == 0:
+            monsters.update()
+        if pygame.sprite.spritecollideany(shp, monsters):
 
-            if c % 20 == 0:
-                monsters.update()
+            hp -= 1
+            many_bombs.empty()
+            monsters.empty()
+            shp.relocation()
+            for s in range(rows - 1):
+                for i in range(22):
+                    m = mobs(screen)
+                    m.x = wd + wd * i
+                    m.y = ht + ht * s
+                    m.rect.x = m.x
+                    m.rect.y = m.y
+                    monsters.add(m)
+            if hp <= 0:
+                start_screen()
+                hp = 3
+        if len(monsters) == 0:
 
-            if pygame.sprite.spritecollideany(shp, monsters):
-                hp -= 1
-                many_bombs.empty()
-                monsters.empty()
-                shp.relocation()
-                for s in range(5):
-                    for i in range(22):
-                        m = mobs(screen)
-                        m.x = wd + wd * i
-                        m.y = ht + ht * s
-                        m.rect.x = m.x
-                        m.rect.y = m.y
-                        monsters.add(m)
-
-                if hp == 0:
-
-                    start_screen()
-            if len(monsters) ==0:
-                for s in range(random.randint(1, 7)):
-                    for i in range(random.randint(1, 22)):
-                        m = mobs(screen)
-                        m.x = wd + wd * i
-                        m.y = ht + ht * s
-                        m.rect.x = m.x
-                        m.rect.y = m.y
-                        monsters.add(m)
-            monsters.draw(screen)
-            for i in monsters.sprites():
-                if i.rect.bottom == screen.get_rect().bottom:
-                    start_screen()
-            pygame.display.flip()
-            screen.blit(bg, (0, 0))
-            text1 = f1.render(f'жизни:{hp}', True,
-                              (180, 0, 0))
-            screen.blit(text1, (0, 0))
-            c += 1
-            print(running)
-        pygame.quit()
+            for s in range(rows):
+                for i in range(22):
+                    m = mobs(screen)
+                    m.x = wd + wd * i
+                    m.y = ht + ht * s
+                    m.rect.x = m.x
+                    m.rect.y = m.y
+                    monsters.add(m)
+            rows += 1
+        print(len(monsters))
+        monsters.draw(screen)
+        for i in monsters.sprites():
+            if i.rect.bottom == screen.get_rect().bottom:
+                hp = 3
+                start_screen()
+        hl = health(screen, hp)
+        hl.realise()
+        pygame.display.flip()
+        screen.blit(bg, (0, 0))
+       
+        c += 1
+    exit()
